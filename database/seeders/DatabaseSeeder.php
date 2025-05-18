ee5c6ad06b2c37add4869106088a0f303e513b68
Nuno Correia <nuno_correia_96@hotmail.com>
2025-05-18 17:10:04 +0200

    Add job creation and listing functionality with pagination

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Employer;
 use App\Models\Job;
-use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Database\Factories\EmployerFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,32 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create some tags first
-        $tags = Tag::factory()->createMany([
-            ['name' => 'Laravel'],
-            ['name' => 'PHP'],
-            ['name' => 'JavaScript'],
-            ['name' => 'Vue.js'],
-            ['name' => 'React'],
-            ['name' => 'Full-Stack'],
-            ['name' => 'Backend'],
-            ['name' => 'Frontend'],
-            ['name' => 'Remote'],
-            ['name' => 'Senior']
+        User::factory()->create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'test@example.com',
         ]);
 
-        // Create 10 employers
-        $employers = Employer::factory(10)->create();
-
-        // Create 20 jobs and attach random tags to each
-        $jobs = Job::factory(20)
-            ->recycle($employers)
-            ->create()
-            ->each(function ($job) use ($tags) {
-                // Attach 2-4 random tags to each job
-                $job->tags()->attach(
-                    $tags->random(rand(2, 4))->pluck('id')->toArray()
-                );
-            });
+        $this->call(JobSeeder::class);
     }
 }
