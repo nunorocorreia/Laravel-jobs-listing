76b7c923c22c0eae878436182bcf1cd14fd5cb57
Nuno Correia <nuno_correia_96@hotmail.com>
2025-05-19 18:14:39 +0200

    Refactor job routes and views, add JobController
    Replaced inline route logic with a dedicated JobController to manage job-related actions. Updated routes to use `Route::resource` and moved job views to be dynamically handled via controller methods, removing redundant blade files for improved maintainability.

--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,12 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+Route::resource('jobs', JobController::class);
 
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.show', ['job' => $job]);
-});
-
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
 
 
